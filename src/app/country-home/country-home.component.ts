927e30a0dcba296aec28951bf08215cb607888b0
Ramaj57 <slamaster02@yahoo.com>
2025-01-13 19:15:30 -0500

    Initial setup

---
+++ src/app/country-home/country-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-home.component.html',
+  styleUrl: './country-home.component.css'
+})
+export class CountryHomeComponent {
+
+}
