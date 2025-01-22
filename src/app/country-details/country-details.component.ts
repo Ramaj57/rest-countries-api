5d436f9d620db70cf486959b9474ff90bb016899
Ramaj57 <slamaster02@yahoo.com>
2025-01-21 22:08:13 -0500

    added inputs to country details component

--- src/app/country-details/country-details.component.ts
+++ src/app/country-details/country-details.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-country-details',
   standalone: true,
   imports: [],
   templateUrl: './country-details.component.html',
-  styleUrl: './country-details.component.css'
+  styleUrl: './country-details.component.css',
 })
 export class CountryDetailsComponent {
-
+  name = input<string>('');
+  nativeName = input<string>('');
+  population = input<number>();
+  region = input<string>('');
+  subregion = input<string>('');
+  capital = input<string>();
+  topLevelDomain = input<[string]>();
+  currencies = input<string>();
+  languages = input<string>('');
+  borders = input<string>('');
 }
