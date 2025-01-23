b2aa8abdd4d3a2536954fdf70a4d912de1cecfae
Ramaj57 <slamaster02@yahoo.com>
2025-01-22 21:11:43 -0500

    small amount of css for dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'rest-countries-api';
+  isDark = signal<boolean>(false);
+
+  toggleDarkMode() {
+    if (!this.isDark()) {
+      this.isDark.set(true);
+      document.body.classList.toggle('dark');
+    } else {
+      this.isDark.set(false);
+      document.body.classList.toggle('dark');
+    }
+  }
 }
