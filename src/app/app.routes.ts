8f0daf690bc8086a9af7d5a13fa614e1aff89478
Ramaj57 <slamaster02@yahoo.com>
2025-01-26 22:37:01 -0500

    added initial thoughts on routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { CountryHomeComponent } from './country-home/country-home.component';
+import { CountryDetailsComponent } from './country-details/country-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component: CountryHomeComponent,
+    },
+    {
+        path:'home', component:CountryHomeComponent,
+        children:[
+            {
+                path:':place', component: CountryDetailsComponent,
+            
+            }
+        ]
+            
+    },
+    
+];
