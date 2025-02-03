46da202ffbbbf4f0d3b64d0e05d70457b3af6223
Ramaj57 <slamaster02@yahoo.com>
2025-02-03 11:25:13 -0500

    changed names of methods

--- src/app/countries.service.ts
+++ src/app/countries.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { Country } from './countries-model';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,14 +9,18 @@ import { HttpClient } from '@angular/common/http';
 export class CountriesService {
   allCountries = signal<Country[]>([]);
   private httpClient = inject(HttpClient);
-  
-    fetchCountryData(url: string) {
-      return this.httpClient.get<Country[]>(url);
-    }
-  
-    loadHomePage() {
-      return this.fetchCountryData('https://restcountries.com/v3.1/all')
-    }
 
+  fetchCountryData(url: string) {
+    return this.httpClient.get<Country[]>(url);
   }
 
+  getAllCountries() {
+    return this.fetchCountryData('https://restcountries.com/v3.1/all');
+  }
+
+  getCountryByName(name:string) {
+    return this.fetchCountryData(
+      `https://restcountries.com/v3.1/name/${name}?fullText=true`
+    ).pipe(map(([resData]) => resData));
+  }
+}
