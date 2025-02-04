5cba358c375e244ab8a1535d7f350f1d42a2b75c
Ramaj57 <slamaster02@yahoo.com>
2025-02-04 16:16:11 -0500

    set up route changed from signals and tried to set up filtering in the input

--- src/app/country-home/country-home.component.ts
+++ src/app/country-home/country-home.component.ts
@@ -1,30 +1,45 @@
-import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { Country } from '../countries-model';
 import { HomeCardComponent } from '../home-card/home-card.component';
 import { CountriesService } from '../countries.service';
-import { SelectComponent } from "../select/select.component";
+import { SelectComponent } from '../select/select.component';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FormatBigNumbersPipe } from "../format-big-numbers.pipe";
 
 @Component({
   selector: 'app-country-home',
   standalone: true,
-  imports: [HomeCardComponent, SelectComponent],
+  imports: [HomeCardComponent, SelectComponent, RouterLink, FormsModule],
   templateUrl: './country-home.component.html',
   styleUrl: './country-home.component.css',
 })
 export class CountryHomeComponent implements OnInit {
-  countries = signal<Country[]>([]);
+  searchFilter: string = '';
+  countries: Country[] = [];
   private countriesService = inject(CountriesService);
   private destroyRef = inject(DestroyRef);
+  countriesList: Country[] = [];
 
   ngOnInit(): void {
-    const subscription = this.countriesService.loadHomePage().subscribe({
+    const subscription = this.countriesService.getAllCountries().subscribe({
       next: (resData) => {
-        this.countries.set(resData);
+        this.countries = resData;
       },
     });
+
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
-    console.log(subscription);
+  }
+
+  get filteredCountries() {
+    return this.countries.filter(country => country.name.common.toLowerCase().includes(this.searchFilter.toLowerCase()))
   }
 }
+
