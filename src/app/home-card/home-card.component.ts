0fd224acaf993b2e601afdd1e616b658efa1c327
Ramaj57 <slamaster02@yahoo.com>
2025-02-04 16:14:37 -0500

    added inputs

--- src/app/home-card/home-card.component.ts
+++ src/app/home-card/home-card.component.ts
@@ -1,18 +1,24 @@
 import { Component, computed, input, signal } from '@angular/core';
 import { Country } from '../countries-model';
+import { FormatBigNumbersPipe } from "../format-big-numbers.pipe";
 
 @Component({
   selector: 'app-home-card',
   standalone: true,
-  imports: [],
+  imports: [FormatBigNumbersPipe],
   templateUrl: './home-card.component.html',
   styleUrl: './home-card.component.css',
 })
 export class HomeCardComponent {
-  countries = signal<Country[]>([]);
   name = input<string>('');
-  population = input<number>();
+  population = input<string>();
   region = input<string>('');
+  nativeName = input<string>('');
+subregion = input<string>('');
+topLevelDomain = input<[string]>();
+currencies = input<string>();
+languages = input<string>('');
+borders = input<string>('');
   capital = input<string[] | undefined>(['']);
   flag = input<string>();
   alt = computed(() => 'picture of ' + this.name() + "'s flag");
