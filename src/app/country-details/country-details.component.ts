d6bc18615c7b6498a1aba4277218929b847c0ab2
Ramaj57 <slamaster02@yahoo.com>
2025-02-04 22:32:55 -0500

    tried to have fallback text for all possibly undefined properties

--- src/app/country-details/country-details.component.ts
+++ src/app/country-details/country-details.component.ts
@@ -38,16 +38,20 @@ export class CountryDetailsComponent {
         .pipe(tap((res) => console.log(res)));
       this.country$.subscribe((country) => {
         // Update signals with country data
-        this.nativeName.set(country.name.nativeName); // Fallback in case the native name is missing
+        if(country.name.nativeName)
+          this.nativeName.set(country.name.nativeName || 'N/A');
+         // Fallback in case the native name is missing
         this.image.set(country.flags.png);
-        this.population.set(country.population || '');
-        this.region.set(country.region || '');
-        this.capital?.set(country.capital || ['']); // Assuming capital is an array
-        this.topLevelDomain?.set(country.tld || ['']); // Assuming tld is an array
-        this.currencies?.set(country.currencies); // Assuming currencies is an array of objects
-        this.languages.set(country.languages); // Join languages into a string if multiple
-        this.name.set(country.name.common || '');
-        this.subregion.set(country.subregion || 'Does not have a subregion');
+        this.population.set(country.population || 'N/A');
+        this.region.set(country.region || 'N/A');
+        this.capital.set(country.capital || ['N/A']); // Assuming capital is an array
+        this.topLevelDomain?.set(country.tld || ['N/A']); // Assuming tld is an array
+        if(country.currencies)
+        this.currencies.set(country.currencies || 'N/A');
+      if(country.languages) // Assuming currencies is an array of objects
+        this.languages.set(country.languages || 'N/A'); // Join languages into a string if multiple
+        this.name.set(country.name.common || 'N/A');
+        this.subregion.set(country.subregion || 'N/A');
       });
 
       const subscription = this.countriesService
@@ -73,6 +77,6 @@ export class CountryDetailsComponent {
   }
 
   displayNativeName(nativeName: NativeName) {
-    return Object.values(nativeName).map((orginal) => orginal.official);
+    return Object.values(nativeName);
   }
 }
