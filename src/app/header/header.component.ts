e02d240b186ab80a510dbe45e394c5614f64fd5e
Ramaj57 <slamaster02@yahoo.com>
2025-02-19 18:00:48 -0500

    styling header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,20 @@
+import { NgClass } from '@angular/common';
 import { Component, output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   toggle = output<void>();
+  isDark = false;
+
+  toggleBodyDarkMode() {
+    this.isDark = !this.isDark;
+  }
 
   onToggleDark() {
     this.toggle.emit();
