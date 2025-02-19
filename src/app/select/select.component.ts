48cb5efa170955a0162a86688d9326e0253b7924
Ramaj57 <slamaster02@yahoo.com>
2025-02-19 18:01:13 -0500

    styling select component

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,9 +1,10 @@
+import { NgClass } from '@angular/common';
 import { Component, input,output } from '@angular/core';
 
 @Component({
   selector: 'app-select',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './select.component.html',
   styleUrl: './select.component.css'
 })
@@ -11,9 +12,13 @@ export class SelectComponent {
 options = input<string[]>();
 valueChange = output<string>();
 value = input<string>();
-
+isDark = false;
 
 onSelect(e:any) {
   this.valueChange.emit(e.target.value)
+}
+
+toggleBodyDarkMode(){
+  this.isDark = !this.isDark;
 }
 }
